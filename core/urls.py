bf3b5dadceb6b08b2f55b21c467976efbfe59e23
Семён <Semen8691>
2022-05-10 15:10:33 +1000

    register,login

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,15 @@
+
 from django.urls import path
-from core.views import index
+from core.views import index, register, logout
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     # 1-путь,2-функция,3-кр.название
+
     path('', index, name='index'),
-    # path('form/', form, name='form'),
-    # path('register/', register, name='register'),
-    # path('login/', LoginView.as_view(), name='login'),
-    # path('logout/', logout, name='logout'),
-    # path('mainpage/', mainpage, name='mp')
+    path('register/', register, name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', logout, name='logout')
+
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
 
+from django.shortcuts import render
+from core.forms import UserRegistrationForm
+from django.contrib.auth.views import auth_logout
 
 def index(reguest):
     '''
      Это функция отрисовки гл.страницы
     '''
     return render(reguest, 'index.html')
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save()
+            new_user.set_password(user_form.cleaned_data['password_1'])
+            new_user.save()
+            return render(request, 'login.html', {'new_user': new_user})
+    user_form = UserRegistrationForm()
+    return render(request, 'register.html', {'user_form': user_form})
+
+
+def logout(request):
+    auth_logout(request)
+    return render(request, 'index.html')
