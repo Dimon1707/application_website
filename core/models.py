303a48a8f754f1996cb333bbc0c8e999ea6e6f9b
Семён <Semen8691>
2022-05-10 22:51:22 +1000

    model

--- core/models.py
+++ core/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Model
 
 class CustomUser(User):
     age = models.PositiveIntegerField(
 
+    )
+
+class Applications(models.Model):
+    name = models.CharField(
+        max_lenght = 50
+    )
+    email = models.EmailField(
+        max_length=255,
+        unique=True
+    )
+    phone = models.IntegerField(
+        max_lenght = 50
+    )
+    date =  models.DateTimeField(
+        auto_now=True
     )
